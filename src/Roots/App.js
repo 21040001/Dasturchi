bbb831abd6b6438ca1b0db301ce58373ccf03cf1
Davronbek Abdurazzokov <abdurazzoqovdavronbek3@gmail.com>
2025-03-24 05:30:43 +0300

    .

--- src/Roots/App.js
+++ src/Roots/App.js
@@ -16,7 +16,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="blogs" element={<BlogPage />} />
+          <Route path="blogs" element={<NoPage />} />
           <Route path="course" element={<CoursePage />} />
           <Route path="read" element={<ReadPage />} />
           <Route path="contactMe" element={<Contact />} />
